a3f327fad1bd096fbed11ffb8d1d093f75c26672
KadenKent <kentkd@fcpsstudent.us>
2025-03-24 13:24:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.markdown("<h1 style='text-align: center;'>Escape Zaroff's Castle</h1>", unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center; >Answer questions to get the code and escape the castle.</h1>", unsafe_allow_html=true)
+st.markdown("<h2 style='text-align: center; >Answer questions to get the code and escape the castle.</h2>", unsafe_allow_html=True)
