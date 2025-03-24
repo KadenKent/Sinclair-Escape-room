9a62a4c03ea930d2efa54b4daf8b5cb37ce4a0a3
KadenKent <kentkd@fcpsstudent.us>
2025-03-24 13:14:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.title("Welcome to Zaroff's castle")
-st.subheader(<center>"Answer Questions to get the code to escape the castle.")
+st.subheader("<h1 style='text-align: center;'>Hello, World!</h1>", unsafe_allow_html=True)
