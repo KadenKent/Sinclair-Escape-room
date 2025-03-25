33da0885fd3096289dcb1090c14467ab248d78a2
KadenKent <kentkd@fcpsstudent.us>
2025-03-25 14:55:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,33 +4,39 @@ st.markdown("<h1 style='text-align: center;'>Escape Zaroff's Castle</h1>", unsaf
 st.markdown("<h3 style='text-align: center;'>Answer quetions to unlock the code and uncipher the code.</h3>", unsafe_allow_html=True)
 
 # Question 1
-st.header("1. What is  short and sharp as a word?")
+st.text("1. What is  short and sharp as a word?")
 options_1 = ['Staccato', 'Palpable', 'Extermity', 'Scruples']
 answer_1 = st.radio("Staccato:", options_1)
 #Question 2
-st.header("2. What is a loosely curled fur made from lamb skin?")
+st.text("2. What is a loosely curled fur made from lamb skin?")
 option_2 = ['Capital','Droll','Analytical','Astrakhan']
 answer_2 = st.radio("Astrakhan:", option_2)
 #Question 3
-st.header("3. What is the author's purpose?")
+st.text("3. What is the author's purpose?")
 option_3 = ['Entertain','Inform','Presuade','Describe']
 answer_3 = st.radio("Entertain:", option_3)
+#Question 4
+st.text("4. What did Zaroff shoot while Rainsford was in bed?".)
+option_4 = ['A human','A deer','A cape buffalo','A dog']
+answer_4 = st.radio("A dog:", option_4)
 
 # Add a submit_button
 submit_button = st.button("Submit")
 if submit_button:
     if answer_1 == "Staccato":
-        st.subheader("Correct! Here is a hint, Caesar Cipher")
+        st.text("Correct! Here is a hint, Caesar Cipher")
     else:
-        st.subheader("Incorrect. try again.")
+        st.text("Incorrect. try again.")
         
     if answer_2 == "Astrakhan":
-            st.subheader("Correct! decipher this code, abzivlml")
+            st.text("Correct! decipher this code, abzivlml")
     else:
-        st.subheader("Incorrect. try again.")
+        st.text("Incorrect. try again.")
     if answer_3 == "Entertain":
-        st.subheader("Correct! Decipher this code, cpnlulyl")
+        st.text("Correct! Decipher this code, cpnlulyl")
     else:
-        st.subheader("Incorrect. try again.")
-
-
+        st.text("Incorrect. try again.")
+        if answer_4 == "A dog":
+            st.text("Correct! Decipher this code, Qhl fvqh d acxj evwh vvh pabmlkz KSZVXXF ooac vlx bquulkz 3,7,6,2,5,1,4")
+        else:
+            st.text("Incorrect. try again.")
