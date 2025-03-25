41667f5df50111c9b8f8d790d00724db7ad1497a
KadenKent <kentkd@fcpsstudent.us>
2025-03-25 11:56:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import streamlit as st
 # Title and subtitle code
 st.markdown("<h1 style='text-align: center;'>Escape Zaroff's Castle</h1>", unsafe_allow_html=True)
 st.markdown("<h2 style='text-align: center;'>Answer quetions to unlock the code and uncipher the code.</h2>", unsafe_allow_html=True)
-#background color
+# Background color
 st.markdown("""
     <style>
         body {
-            background-color: #00008B;  # Choose your desired background color
+            background-color: #87CEFA;  # Light Sky Blue color
         }
     </style>
 """, unsafe_allow_html=True)
