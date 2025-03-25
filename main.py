25e98c67ecabfd4f811006f7894d6fbfbe436d08
KadenKent <kentkd@fcpsstudent.us>
2025-03-25 11:47:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import streamlit as st
+# Title and subtitle code
 st.markdown("<h1 style='text-align: center;'>Escape Zaroff's Castle</h1>", unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center;'>Answer quetions to unlock the code and uncipher the code..</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;'>Answer quetions to unlock the code and uncipher the code.</h2>", unsafe_allow_html=True)
+#background color
+st.markdown("""
+  <style>
+    body {
+      background-color: #f0f0f0;
+    }
+  </style>
+""", unsafe_allow_html=True)
