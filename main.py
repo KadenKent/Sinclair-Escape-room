2b2afe38ff1e431de934e359132bc5112cec712a
KadenKent <kentkd@fcpsstudent.us>
2025-03-25 13:01:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ options_1 = ['Staccato', 'Palpable', 'Extermity', 'Scruples']
 answer_1 = st.radio("scruples:", options_1)
 
 # Add a submit_button
-    if answer_1 == "Staccato":
+if answer_1 == "Staccato":
     st.subheader("Correct! Here is a hint, Caesar Cipher")
     else:
         st.subheader("Incorrect. try again!")
