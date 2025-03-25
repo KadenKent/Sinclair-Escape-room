5567ac41b532bae0f4e5ea98c054a05b8c3897b1
KadenKent <kentkd@fcpsstudent.us>
2025-03-25 11:53:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ st.markdown("<h1 style='text-align: center;'>Escape Zaroff's Castle</h1>", unsaf
 st.markdown("<h2 style='text-align: center;'>Answer quetions to unlock the code and uncipher the code.</h2>", unsafe_allow_html=True)
 #background color
 st.markdown("""
-  <style>
-    body {
-      background-color: #00008B;
-    }
-  </style>
+    <style>
+        body {
+            background-color: #f0f0f0;  # Choose your desired background color
+        }
+    </style>
 """, unsafe_allow_html=True)
