828eb70e2b93df028f9de1933c7348485ea2c1de
KadenKent <kentkd@fcpsstudent.us>
2025-03-26 12:42:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ option_6 ('Cigar','His watch','A coin','his shoe')
 answer_6 = st.radio("",option_6)
 #Question 7
 st.text("7. What cracked Zaroff\'s rib cage?")
-option_7('his dog','an elk',a moose','a cape buffalo')
+option_7('his dog','an elk','a moose','a cape buffalo')
 answer_7 = st.radio("",option_7)
 #Question 8
 st.text("")
